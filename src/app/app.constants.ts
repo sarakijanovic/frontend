93849a212e7c126207808300610a15e9c90e9800
sarakijanovic <sara.kijanovic@unc.ac.rs>
2021-06-13 11:08:08 +0200

    Linkovi

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,3 @@
-//ovde cemo smestiti vrednosti konstanti
 /*
 export const SMER_URL = 'https://rva-backendit142018.herokuapp.com/smer';
 export const PROJEKAT_URL = 'https://rva-backendit142018.herokuapp.com/projekat';
@@ -6,9 +5,9 @@ export const GRUPA_URL = 'https://rva-backendit142018.herokuapp.com/grupa';
 export const STUDENT_URL = 'https://rva-backendit142018.herokuapp.com/student';
 export const STUDENTI_GRUPE = 'https://rva-backendit142018.herokuapp.com/studentiGrupa';
 */
-export const SMER_URL = 'http://localhost:8083/smer';
-export const PROJEKAT_URL = 'http://localhost:8083/projekat';
-export const GRUPA_URL = 'http://localhost:8083/grupa';
-export const STUDENT_URL = 'http://localhost:8083/student';
-export const STUDENTI_GRUPE = 'http://localhost:8083/studentiGrupa';
+export const SMER_URL = 'https://rva-bekend-gr2.herokuapp.com/smer';
+export const PROJEKAT_URL = 'https://rva-bekend-gr2.herokuapp.com/projekat';
+export const GRUPA_URL = 'https://rva-bekend-gr2.herokuapp.com/grupa';
+export const STUDENT_URL = 'https://rva-bekend-gr2.herokuapp.com/student';
+export const STUDENTI_GRUPE = 'https://rva-bekend-gr2.herokuapp.comstudentiGrupa';
 
